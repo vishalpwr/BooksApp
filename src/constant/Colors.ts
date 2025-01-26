61a65596b3622f309d3d55bb1337cfd35d324560
vishalpwr <vshalpwr@gmail.com>
2025-01-26 17:03:18 +0530

    colors

---
+++ src/constant/Colors.ts
@@ -0,0 +1,47 @@
+export default {
+  primary: '#007aff',
+  background: '#F7F9FC',
+  darkBackground: '#121212',
+  black: '#212121',
+  gray: '#B0B0B0',
+  white: '#FFFFFF',
+  gold: '#FFD700',
+  discount: '#FF5A5F',
+  teal: '#4ADE80',
+  orange: '#FF9800',
+  details: '#dae5f5',
+  red: '#FF4D4D',
+  lightRed: '#FFE5E5',
+  lightGray: '#E0E0E0',
+
+  lightText: "#121212",
+  darkText: "#ffffff",
+  borderColor: "#dddddd",
+  switchTrack: "#a0c9fa",
+  inactiveSwitchThumb: "#f4f3f4",
+
+  gray0: '#E7EEFA',
+  gray1: '#F7FAFC',
+  gray2: '#EDF2F7',
+  gray3: '#E2E8F0',
+  gray4: '#CBD5E0',
+  gray5: '#A0AEC0',
+
+  LightMode: '#F5F5F5',
+  DarkMode: '#1E1E1E',
+  // •	Light Mode:
+  text1: '#212121', //(Dark Grey)
+  subtext1: '#757575', //(Grey)
+  // •	Dark Mode:
+  text2: '#E0E0E0', //(Light Grey)
+  subtext2: '#BDBDBD', //(Muted Grey)
+
+  input1: '#F5F5F5',
+  border1: '#BDBDBD.',
+  input2: '#1E1E1E',
+  border2: '#333333.',
+
+  dark: '#000',
+  light: '#FFFFFF',
+
+}
