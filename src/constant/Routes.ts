baf5b68aa25e162f70e391ed5ea3be9b95a0ec20
vishalpwr <vshalpwr@gmail.com>
2025-01-26 17:03:29 +0530

    add constants

---
+++ src/constant/Routes.ts
@@ -0,0 +1,11 @@
+export default {
+  signup: 'SignUp',
+  signin: 'Signin',
+  home: 'Home',
+  settings: 'Settings',
+  wishlist: 'Wishlist',
+  cart: 'Cart',
+  details: 'Details',
+  reading: 'Reading',
+  bottomTab: 'BottomTab',
+}
---
+++ src/constant/constants.ts
@@ -0,0 +1,9 @@
+export const size = {
+  vSmall: 8,
+  small: 10,
+  mid: 16,
+  large: 20,
+  mLarge: 24,
+  xLarge: 28,
+  xxLarge: 32,
+}
